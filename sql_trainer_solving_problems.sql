711331cf6aea204e1677e8c6ec0e39dea0691d24
ZharkovaAV <72766172+ZharkovaAV@users.noreply.github.com>
2026-03-01 21:10:00 +0400

    Update and rename SQL Trainer: Solving Problems to sql_trainer_solving_problems.sql

--- sql_trainer_solving_problems.sql
+++ sql_trainer_solving_problems.sql
@@ -1,57 +1,69 @@
+/*
 Задание #1
 Имена всех людей
 Вывести имена всех людей, которые есть в базе данных авиакомпаний
 Поля в результирующей таблице:
 name
+*/
 
 SELECT name
 FROM Passenger
 
+/*
 Задание #2
 Названия всех авиакомпаний
 Вывести названия всеx авиакомпаний
 Поля в результирующей таблице:
 name
+*/
 
 SELECT name
 FROM Company
 
+/*
 Задание #3
 Рейсы из Москвы
 Вывести все рейсы, совершенные из Москвы
 Поля в результирующей таблице:
 *
+*/
 
 SELECT *
 FROM Trip
 WHERE town_from = 'Moscow'
 
+/*
 Задание #4
 Имена, заканчивающиеся на "man"
 Вывести имена людей, которые заканчиваются на "man"
 Поля в результирующей таблице:
 name
+*/
 
 SELECT name
 FROM Passenger
 WHERE name LIKE '%man'
 
+/*
 Задание #5
 Количество рейсов на TU-134
 Вывести количество рейсов, совершенных на TU-134
 Используйте конструкцию "as count" для агрегатной функции подсчета количества рейсов. Это необходимо для корректной проверки.
 Поля в результирующей таблице:
 count
+*/
 
 SELECT COUNT(*) AS COUNT
 FROM Trip
 WHERE plane = 'TU-134'
 
+/*
 Задание #6
 Компании, летавшие на Boeing
 Какие компании совершали перелеты на Boeing
 Поля в результирующей таблице:
 name
+*/
 
 SELECT Company.name
 FROM Trip
@@ -59,17 +71,20 @@ FROM Trip
 WHERE plane = 'Boeing'
 GROUP BY company
 
+/*
 Задание #7
 Самолеты, летящие в Москву
 Вывести все названия самолётов, на которых можно улететь в Москву (Moscow)
 Поля в результирующей таблице:
 plane
+*/
 
 SELECT plane
 FROM Trip
 WHERE town_to = 'Moscow'
 GROUP BY plane
 
+/*
 Задание #8
 Полёты из Парижа
 В какие города можно улететь из Парижа (Paris) и сколько времени это займёт?
@@ -78,17 +93,20 @@ GROUP BY plane
 Поля в результирующей таблице:
 town_to
 flight_time
+*/
 
 SELECT town_to,
 	TIMEDIFF(time_in, time_out) AS flight_time
 FROM Trip
 WHERE town_from = 'Paris'
 
+/*
 Задание #9
 Компании с рейсами из Владивостока
 Какие компании организуют перелеты из Владивостока (Vladivostok)?
 Поля в результирующей таблице:
 name
+*/
 
 SELECT Company.name
 FROM Trip
@@ -96,20 +114,25 @@ FROM Trip
 WHERE town_from = 'Vladivostok'
 GROUP BY Company.name
 
+/*
 Задание #10
 Вылеты в определенное время
 Вывести вылеты, совершенные с 10 ч. по 14 ч. 1 января 1900 г.
 Поля в результирующей таблице:
 *
+*/
 
 SELECT *
 FROM Trip
 WHERE time_out BETWEEN '1900-01-01T10:00:00.000Z' AND '1900-01-01T14:00:00.000Z'
 
+/*
+Задание #11
 Пассажиры с самым длинным ФИО
 Выведите пассажиров с самым длинным ФИО. Пробелы, дефисы и точки считаются частью имени.
 Поля в результирующей таблице:
 name
+*/
 
 SELECT name
 FROM passenger
@@ -117,6 +140,7 @@ WHERE LENGTH(name) = (
     SELECT MAX(LENGTH(name))
     FROM passenger)
 
+/*
 Задание #12
 Количество пассажиров на рейсах
 Выведите идентификаторы всех рейсов и количество пассажиров на них. Обратите внимание, что на каких-то рейсах пассажиров может не быть. В этом случае выведите число "0".
@@ -124,28 +148,33 @@ WHERE LENGTH(name) = (
 Поля в результирующей таблице:
 id
 count
+*/
 
 SELECT Trip.id, COUNT(Pass_in_trip.id) AS count
 FROM Trip
 LEFT JOIN Pass_in_trip ON Trip.id = Pass_in_trip.trip
 GROUP BY Trip.id
 
+/*
 Задание #13
 Полные тёзки
 Вывести имена людей, у которых есть полный тёзка среди пассажиров
 Поля в результирующей таблице:
 name
+*/
 
 SELECT name
 FROM Passenger
 GROUP BY name
 HAVING COUNT(name) > 1
 
+/*
 Задание #14
 Города, которые посетил Bruce Willis
 В какие города летал Bruce Willis
 Поля в результирующей таблице:
 town_to
+*/
 
 SELECT town_to
 FROM Passenger
@@ -153,12 +182,14 @@ FROM Passenger
 	JOIN trip ON trip.id = Pass_in_trip.trip
 WHERE name = 'Bruce Willis'
 
+/*
 Задание #15
 Прибытие Steve Martin в Лондон
 Выведите идентификатор пассажира Стив Мартин (Steve Martin) и дату и время его прилёта в Лондон (London)
 Поля в результирующей таблице:
 id
 time_in
+*/
 
 SELECT Passenger.id,
 	Trip.time_in
@@ -168,6 +199,7 @@ FROM Passenger
 WHERE Passenger.name = 'Steve Martin'
 	AND Trip.town_to = 'London'
 
+/*
 Задание #16
 Сортировка пассажиров по количеству полетов
 Вывести отсортированный по количеству перелетов (по убыванию) и имени (по возрастанию) список пассажиров, совершивших хотя бы 1 полет.
@@ -175,7 +207,8 @@ WHERE Passenger.name = 'Steve Martin'
 Поля в результирующей таблице:
 name
 count
-
+*/
+	
 SELECT name,
 	COUNT(name) AS COUNT
 FROM Passenger
@@ -185,6 +218,7 @@ GROUP BY name
 ORDER BY COUNT DESC,
 	name ASC
 
+/*
 Задание #17
 Траты членов семьи в 2005 году
 Определить, сколько потратил в 2005 году каждый из членов семьи. В результирующей выборке не выводите тех членов семьи, которые ничего не потратили.
@@ -193,6 +227,7 @@ ORDER BY COUNT DESC,
 member_name
 status
 costs
+*/
 
 SELECT member_name,
 	STATUS,
@@ -203,11 +238,13 @@ WHERE YEAR(DATE) = 2005
 GROUP BY member_name,
 	STATUS
 
+/*
 Задание #18
 Самый старший человек
 Выведите имя самого старшего человека. Если таких несколько, то выведите их всех.
 Поля в результирующей таблице:
 member_name
+*/
 
 SELECT member_name
 FROM FamilyMembers
